3e1f726864d3b4006e4e159d932d11afdeb2dc53
Scott <chiang_chun_wei@hotmail.com>
2014-03-28 10:35:55 +0800

    I put a comment

--- src/com/example/serverclientroom/Grid.java
+++ src/com/example/serverclientroom/Grid.java
@@ -7,7 +7,7 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class Grid extends Activity {
-	
+	// We don't do much here
 	private TextView tv;
 	private Handler updateConversationHandler;
 
